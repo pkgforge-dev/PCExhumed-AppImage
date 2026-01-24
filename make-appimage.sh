9398c8eae4e71a04353a97cb45aa50f56feb5746
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:10:17 -0300

    remove pipewire

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/pixmaps/pcexhumed.icns
 export DESKTOP=/usr/share/applications/pcexhumed.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/pcexhumed
